3ef44195b5c0ef3c6fb840c96343cb4cb4f12f2a
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-05-21 23:45:31 +0530

    day 3 practice

--- practice/sandbox/template/vite.config.js
+++ practice/sandbox/template/vite.config.js
@@ -1,20 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
-  host: '0.0.0.0',
+
   server: {
+    host: '0.0.0.0',
     allowedHosts: true,
     port: 5173,
+
     watch: {
       usePolling: true,
       interval: 300
     },
+
     hmr: {
       clientPort: 5173
     }
