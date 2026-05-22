6b93a52519b20e2db2eba05823733d4ae47acc7a
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-05-23 02:32:49 +0530

    day 4

--- practice/sandbox/server/src/kubernetes/config.js
+++
@@ -1,8 +0,0 @@
-import * as k8s from '@kubernetes/client-node';
-
-const kc = new k8s.KubeConfig();
-kc.loadFromDefault();
-
-const k8sCoreApi = kc.makeApiClient(k8s.CoreV1Api);
-
-export default k8sCoreApi;
